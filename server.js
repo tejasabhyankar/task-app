2ff4a9335fbdf79235b9841bc0f021fd62356dd0
tejasabhyankar <141743211+tejasabhyankar@users.noreply.github.com>
2026-04-22 10:07:32 +0530

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname));
 
 let projects = [];
@@ -17,12 +17,25 @@ app.get("/projects", (req, res) => {
 // ADD project
 app.post("/projects", (req, res) => {
     const name = req.body.name;
+
     if (name) {
-        projects.push(name);
+        projects.push({ name:name, comments:[]}); 
     }
-    res.send("Added");
+     res.send("Added");
 });
 
+// ADD COMMENT
+app.post("/comment/:id", (req, res) => {
+    const id = req.params.id;
+    const comment = req.body.comment;
+
+    if (projects[id] && comment) {
+        projects[id].comments.push(comment);
+    }
+     res.send("Comment Added");
+});
+
+// DELETE project
 app.delete("/delete/:id", (req, res) => {
     const id = req.params.id;
     projects.splice(id, 1);
