eab6736386dad191980b0267853f641b52cee33d
tejasabhyankar <141743211+tejasabhyankar@users.noreply.github.com>
2026-04-22 10:10:19 +0530

    script.js

--- script.js
+++ script.js
@@ -2,30 +2,46 @@ function loadProjects() {
     fetch("/projects")
     .then(res => res.json())
     .then(data => {
+
         let text = "";
-
         for (let i = 0; i < data.length; i++) {
-            text += data[i] + 
-            " <button onclick='deleteProject(" + i + ")'>Delete</button><br>";
+          text += data[i].name;
+            // comment input
+            text += "<br><input id='c"+i+"'>";
+            text += "<button onclick='addComment("+i+")'>Add</button>";
+            // show comments
+            for (let j = 0; j < data[i].comments.length; j++) {
+                text += "<br>- " + data[i].comments[j];
+            }
+             // delete button 
+            text += "<br><button onclick='deleteProject(" + i + ")'>Delete</button><br><br>";
         }
-
-        document.getElementById("list").innerHTML = text;
+            document.getElementById("list").innerHTML = text;
     });
 }
-
+// add project
 function addProject() {
     let name = document.getElementById("name").value;
-
-    fetch("/projects", {
+     fetch("/projects", {
         method: "POST",
-        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: "name=" + name
-    }).then(() => loadProjects());
+    }).then(loadProjects);
 }
 
+// add comment
+function addComment(i) {
+    let comment = document.getElementById("c"+i).value;
+    fetch("/comment/" + i, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: "comment="+comment
+    }).then(loadProjects);
+}
+// delete project
 function deleteProject(i) {
-    fetch("/delete/" + i, { method: "DELETE" })
-    .then(() => loadProjects());
+    fetch("/delete/"+i, {method:"DELETE"})
+    .then(loadProjects);
 }
-
+// first load
 loadProjects();
